942d0e8593197977809c0f114ef9e36f65494311
JuliaRomaeva <julia.romaeva@gmail.com>
2023-02-12 22:32:43 +0300

    3 цифра числа

---
+++ Example7/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Введите трехзначное число: ");
+int num = int.Parse(Console.ReadLine());
+if(num < 100 || num > 999){
+    Console.WriteLine("Введено неправильное число");
+    return;
+}
+int otvet = num % 10;
+Console.WriteLine($"3 число: {otvet}");
+
